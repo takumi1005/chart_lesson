08021e3760ba90c26c75d6e230c8addd1174a714
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-11-02 08:28:26 +0900

    PiePlotの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LinePlot from './components/LinePlot';
+import PiePlot from './components/PiePlot';
 
 function App() {
   return (
     <div className="App">
       <LinePlot />
+      <PiePlot />
     </div>
   );
 }
