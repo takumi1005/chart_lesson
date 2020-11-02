435df154b60f5f8909ae0ce8e91b6f4bd667b494
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-11-02 09:28:29 +0900

    RadarPlotの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Radar } from 'react-chartjs-2';
 import './App.css';
 import BarPlot from './components/BarPlot';
 import LinePlot from './components/LinePlot';
 import PiePlot from './components/PiePlot';
+import RadarPlot from './components/RadarPlot';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <LinePlot />
       <PiePlot />
       <BarPlot />
+      <RadarPlot />
     </div>
   );
 }
---
+++ src/components/RadarPlot.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Radar } from 'react-chartjs-2'
+
+const data = {
+  labels: [
+    "React",
+    "Vue",
+    "Angular",
+    "JavaScript",
+    "TypeScript",
+    "Redux",
+    "REST API"
+  ],
+  datasets: [
+    {
+      label: "Person A",
+      backgroundColor: "rgba(197, 181, 198, 0.2)",
+      borderColor: "#008b8b",
+      pointBackgroungColor: "#008b8b",
+      pointBorderColor: "#fff",
+      data: [100, 50, 30, 90, 50, 70, 40],
+    },
+    {
+      label: "Person B",
+      backgroundColor: "rgba(197, 181, 198, 0.2)",
+      borderColor: "#ff1493",
+      pointBackgroungColor: "#ff1493",
+      pointBorderColor: "#fff",
+      data: [10, 30, 100, 10, 100, 70, 60],
+    },
+  ],
+};
+
+const RadarPlot: React.FC = () => {
+  return (
+    <div>
+      <Radar data={data} />
+    </div>
+  )
+}
+
+export default RadarPlot
+
