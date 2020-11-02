7e8c1388dd5a864bc8173385a784e3cdc2847f8c
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-11-02 11:14:43 +0900

    MUI buttonの表示

---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const MaterialUI: React.FC = () => {
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        Test Button
+      </Button>
+    </div>
+  )
+}
+
+export default MaterialUI
