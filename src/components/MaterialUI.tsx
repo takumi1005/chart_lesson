a1f5a66e91de504c21d9760b3711b1cbcf06f078
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-11-02 11:44:55 +0900

    Typograghyの使用

--- src/components/MaterialUI.tsx
+++ src/components/MaterialUI.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Button, makeStyles } from '@material-ui/core'
+import { Button, makeStyles, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles ({
   btnStyle: {
     background: "green",
     padding: "3px 50px"
+  },
+  typoStyle: {
+    color: "blue",
   },
 });
 
@@ -12,6 +15,16 @@ const MaterialUI: React.FC = () => {
   const classes = useStyles();
   return (
     <div>
+      <Typography
+        className={classes.typoStyle}
+        color="secondary"
+        variant="h1"
+        align="left"
+        gutterBottom
+        noWrap
+      >
+        Hello UI Hello UIHello UIHello UIHello UIHello UIHello UIHello UI
+      </Typography>
       <Button className={classes.btnStyle} variant="contained" color="primary">
         Test Button
       </Button>
